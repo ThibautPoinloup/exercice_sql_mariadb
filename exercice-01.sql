5e6cb8e3bae4f3051454ef1aab37056da804620f
thibaut-poinloup <poinloupthibaut@gmail.com>
2023-01-04 15:19:57 +0100

    Réponse non corrigé de l'exercice 1

--- exercice-01.sql
+++ exercice-01.sql
@@ -8,39 +8,61 @@ FROM tag;
 
 -- Exo 1.1
 -- Écrivez la requête qui permet de lister tous les students
-
+SELECT * 
+FROM student;
 -- Exo 1.2
 -- Écrivez la requête qui permet de lister tous les projects
-
+SELECT *
+FROM project;
 -- Exo 1.3
 -- Écrivez la requête qui permet de lister le student dont l'id est `2`
-
+SELECT id 
+FROM student
+WHERE id == '2';
 -- Exo 1.4
 -- Écrivez la requête qui permet de lister les students dont l'id n'est pas `2`
-
+SELECT id 
+FROM student
+WHERE id != '2';
 -- Exo 1.5
 -- Écrivez la requête qui permet de lister le project dont l'id est `3`
-
+SELECT id 
+FROM project
+WHERE id == '3';
 -- Exo 1.6
 -- Écrivez la requête qui permet de lister les projects dont l'id n'est pas `3`
-
+SELECT id 
+FROM project
+WHERE id != '3';
 -- Exo 1.7
 -- Écrivez la requête qui permet de lister les students dont l'email contient la chaîne de caractères `.com`
-
+SELECT email
+FROM student
+WHERE email == '.com';
 -- Exo 1.8
 -- Écrivez la requête qui permet de lister les students dont la date de création est antérieure au 10 janvier 2021 inclus
-
+SELECT date
+FROM student
+WHERE date <= DATE_FORMAT("2021-01-10", "%D %b %Y");
 -- Exo 1.9
 -- Écrivez la requête qui permet de lister les projects dont la description contient pas la chaîne de caractères `Dolores`
-
+SELECT descriptions
+FROM project
+WHERE descriptions != 'Dolores';
 -- Exo 1.10
 -- Écrivez la requête qui permet de lister les projects dont la date de création est postérieure au 1er juillet 2021 inclus
-
+SELECT date
+FROM project
+WHERE date >= DATE_FORMAT("2021-07-01", "%D %b %Y");
 -- Exo 1.11
 -- Écrivez la requête qui permet de lister les students ayant un project
 -- Note : les students sans project ont une colonne `project_id` qui est nulle
-
+SELECT student
+FROM project
+WHERE 'project_id' IS NOT NULL; 
 -- Exo 1.12
 -- Écrivez la requête qui permet de lister les students n'ayant pas de project
 -- Note : les students sans project ont une colonne `project_id` qui est nulle
-
+SELECT student
+FROM project
+WHERE 'project_id' IS NULL;
